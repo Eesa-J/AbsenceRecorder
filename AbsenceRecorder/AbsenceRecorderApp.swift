8d58b29a254eeca48d6ff204343fb531bd065800
Eesa Jaswal <jaswal.e@etoncollege.org.uk>
2023-01-31 21:57:17 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Jaswal, Eesa (SPH) on 31/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
