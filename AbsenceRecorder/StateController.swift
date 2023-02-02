d0e4d4847e95a53b1217542f1b4c27be9747aacb
Eesa Jaswal <jaswal.e@etoncollege.org.uk>
2023-02-02 23:51:36 +0000

    Completed tasks in Absence Recorder 3

---
+++ AbsenceRecorder/StateController.swift
@@ -0,0 +1,17 @@
+//
+//  StateController.swift
+//  AbsenceRecorder
+//
+//  Created by Jaswal, Eesa (SPH) on 02/02/2023.
+//
+
+import Foundation
+
+class StateController: ObservableObject {
+    @Published var divisions: [Division]
+    
+    init() {
+        divisions = Division.examples
+    }
+    
+}
---
+++ AbsenceRecorder/View/AbsenceView.swift
@@ -0,0 +1,23 @@
+//
+//  AbsenceView.swift
+//  AbsenceRecorder
+//
+//  Created by Jaswal, Eesa (SPH) on 02/02/2023.
+//
+
+import SwiftUI
+
+struct AbsenceView: View {
+    let division: Division
+    
+    
+    var body: some View {
+        Text("Absence View - \(division.code)")
+    }
+}
+
+struct AbsenceView_Previews: PreviewProvider {
+    static var previews: some View {
+        AbsenceView(division: Division.examples[0])
+    }
+}
